d6ef1268fa2361e52c6e96b28770252d059db40e
Henrique Canto Duarte <hcantoduarte@gmail.com>
2017-12-14 16:24:20 -0200

    Simplified logging procedure

--- Java/CryptoIntegration/CryptoIntegration-ejb/src/main/java/br/com/dojot/mutualauthentication/cryptointegration/messaging/api/LoggingProcessingProducerService.java
+++
@@ -1,8 +0,0 @@
-package br.com.dojot.mutualauthentication.cryptointegration.messaging.api;
-
-import br.com.dojot.mutualauthentication.cryptointegration.beans.dto.LoggingDTO;
-
-public interface LoggingProcessingProducerService {
-	
-	void produce(LoggingDTO dto);
-}
